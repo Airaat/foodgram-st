59125d812d31e6fe2a51bc351330e5cbec4bb59b
Airaat <bigeevjrairatka@gmail.com>
2025-07-05 10:40:19 +0300

    reuse max_image_size const

--- backend/recipes/fields.py
+++ backend/recipes/fields.py
@@ -1,8 +1,10 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as fmt
 import os
 
-MAX_SIZE = 2 * 1024 * 1024  # 2 Mb
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as fmt
+
+from foodgram.settings import MAX_IMAGE_SIZE
+
 
 UNSUPPORTED_FORMAT = 'Недопустимый формат изображения.'
 UNSUPPORTED_IMAGE_SIZE = 'Размер изображения не должен превышать 2MB.'
@@ -14,5 +16,5 @@ def validate_image(image):
     if ext not in ['jpeg', 'jpg', 'png', 'gif']:
         raise ValidationError(fmt(UNSUPPORTED_FORMAT))
 
-    if image.size > MAX_SIZE:
+    if image.size > MAX_IMAGE_SIZE:
         raise ValidationError(fmt(UNSUPPORTED_IMAGE_SIZE))
