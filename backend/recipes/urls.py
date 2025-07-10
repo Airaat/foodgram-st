ad6f6da458e02b70eacd05d453d4013f370eb6d9
Airaat <bigeevjrairatka@gmail.com>
2025-07-10 14:35:55 +0300

    [experimentally] healthcheck for backend service

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import short_link_redirect_view
+from .views import (
+    health_check,
+    short_link_redirect_view,
+)
 
 app_name = 'recipes'
 
 urlpatterns = [
+    path('health', health_check, name='health-check'),
     path('<int:recipe_id>/', short_link_redirect_view, name='short-link-redirect'),
 ]
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from recipes.models import Recipe
 
@@ -9,3 +10,7 @@ def short_link_redirect_view(request, recipe_id):
 
     get_object_or_404(Recipe, id=recipe_id)
     return redirect(f'/recipes/{recipe_id}/')
+
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
